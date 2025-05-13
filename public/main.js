4ad4b01557979b4fed85009c02b7e3b95ec84d30
Tim Etler <tmetler@gmail.com>
2025-05-13 00:17:40 -0700

    Clearing previous answer on submit

--- public/main.js
+++ public/main.js
@@ -48,6 +48,7 @@ let answerText = "";
 // Handle input submission
 searchForm.addEventListener("submit", async (event) => {
   event.preventDefault();
+  answerContainer.innerHTML = "";
   shareButton.style.display = "none";
   userQuery = queryInput.value;
   // Send the LLM request
